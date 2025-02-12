19064039c59a2771fc0bef323e975dbd518d43dd
vishnu_reddy <vishnureddy8980@gmail.com>
2025-02-12 19:44:07 +0530

    Done with waterfall chart related changes

---
+++ src/app/charts/waterfall-chart/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { ParentSize } from "@visx/responsive";
+import { WaterfallChart } from "@/modules/charts/components/WaterFallChart";
+
+const data = [
+  { month: "Jan", earnings: 23 },
+  { month: "Feb", earnings: 18 },
+  { month: "Mar", earnings: -14 },
+  { month: "Apr", earnings: 4 },
+  { month: "May", earnings: -26 },
+  { month: "Jun", earnings: 10 },
+  { month: "Jul", earnings: 32 },
+];
+
+function WaterFallChartComp() {
+  return (
+    <div className="max-w-[1000px] h-[600px]">
+      <ParentSize>
+        {({ width, height }) => (
+          <WaterfallChart
+            width={width}
+            height={height}
+            data={data}
+            xAccessor={(datum) => datum.month}
+            yAccessor={(datum) => datum.earnings}
+            yLabel="Earnings"
+          />
+        )}
+      </ParentSize>
+    </div>
+  );
+}
+
+export default WaterFallChartComp;
